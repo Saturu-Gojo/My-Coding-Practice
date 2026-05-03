1df5b35f78fab3c93d5d400506758412a805fed9
Saturu-Gojo <prashasth068@gmail.com>
2026-05-03 23:57:42 +0530

    Practice

--- 2209/B.cpp
+++ 2209/B.cpp
@@ -8,33 +8,24 @@ int main(){
     while(t--){
         ll n;
         cin>>n;
-        vector<int> v(n),ans(n),s;
-        unordered_map<int,int> mp;
-         unordered_map<int,int> mp2;
+        vector<int> v(n),ans(n);
 
 
         for(int i=0; i<n; i++){
             cin>>v[i];
-            if(mp2.find(v[i])==mp2.end()){
-                s.push_back(v[i]);
-                mp[v[i]]++;
-            }
-
         }
-    
-        sort(s.begin(),s.end());
-
-
-        int val = 0;
-        set<int> st;
 
         for(int i=n-1; i>=0; i--){
-            if(i==n-1){
-                ans[i]=0;
-                st.insert(v[i]);
+            int small = 0;
+            int large = 0;
+            for(int j=i+1; j<n; j++){
+                if(v[i]>v[j])small++;
+                else if(v[j]>v[i])large++;
             }
-
+            ans[i]=max(small,large);
+            
         }
+    
         for(int i=0; i<n; i++){
             cout<<ans[i]<<" ";
         }
